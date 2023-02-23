d8788965dcebbcdc0deded893211d4c07bdc7845
D <dbinod2020@gmail.com>
2023-02-23 22:28:52 +0530

    Day-2 task has submitted

---
+++ src/com/Exception/TransactionException.java
@@ -0,0 +1,15 @@
+package com.Exception;
+
+
+public class TransactionException extends Exception {
+
+	public TransactionException() {
+		super();
+	}
+
+	public TransactionException(String message) {
+		super(message);
+	}
+
+}
+	
---
+++ src/com/Style/ConsoleColor.java
@@ -0,0 +1,5 @@
+package com.Style;
+
+public class ConsoleColor {
+
+}
